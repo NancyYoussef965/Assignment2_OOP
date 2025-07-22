2a656c4fd335167b7966ef8f147a04ab48eb29af
Nancy <ny6049812@gmail.com>
2025-07-22 12:27:54 +0300

    Add project files.

---
+++ Assignment 2 OOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment2_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
